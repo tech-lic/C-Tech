fed9b6e860e781d2ed91b3ddbb5dbf1c35b4b89f
tech-lic <18811441841@163.com>
2017-05-19 16:27:30 +0800

    试试

--- May/src/main/java/com/hi/action/UserAction.java
+++ May/src/main/java/com/hi/action/UserAction.java
@@ -2,57 +2,54 @@ package com.hi.action;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.hi.Dao.UserDao;
 import com.hi.entity.User;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserAction extends ActionSupport {
-private UserDao userDao;
-private List<User> list;
-private User user;	
+    @Autowired
 
+	private UserDao userDao;
+	private List<User> list;
+	private User user;
 
 	public User getUser() {
-	return user;
-}
+		return user;
+	}
 
-
-public void setUser(User user) {
-	this.user = user;
-}
-
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 	public UserDao getUserDao() {
-	return userDao;
-}
+		return userDao;
+	}
 
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
-public void setUserDao(UserDao userDao) {
-	this.userDao = userDao;
-}
+	public List<User> getList() {
+		return list;
+	}
 
+	public void setList(List<User> list) {
+		this.list = list;
+	}
 
-public List<User> getList() {
-	return list;
-}
-
-
-public void setList(List<User> list) {
-	this.list = list;
-}
-
-
-	public String findUser(){
+	public String findUser() {
 		System.out.println("0000");
-		list=userDao.findUser();
+		list = userDao.findUser();
 		System.out.println(list.size());
 		return "find";
 	}
-	
-	public String save(){
+
+	public String save() {
 		System.out.println(user.getName());
-	int a=userDao.save(user);
-	System.out.println(a);
+		int a = userDao.save(user);
+		System.out.println(a);
 		return "save";
 	}
 }
